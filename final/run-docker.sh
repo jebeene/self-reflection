8dbee8deacdf02d02348ab624f432e3513e01578
jebeene <89660618+jebeene@users.noreply.github.com>
2025-04-16 12:26:41 -0400

    docker run

--- final/run-docker.sh
+++ final/run-docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+xhost +local:docker
+
 docker run -it --rm \
   --env DISPLAY=$DISPLAY \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
   --device /dev/video0 \
+  --name reflection \
   reflection
